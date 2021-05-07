c0f294358ffc11e0b644e073c0f19e644c8bcd09
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-05-07 12:00:20 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number,
+    visited: number,
+    deals: number,
+    amount: number,
+    date: string,
+    seller: Seller
+}
+
+export type SalePage = {
+    content?: Sale[],
+    totalPages: number,
+    totalElements: number,
+    last: boolean,
+    numberOfElements?: number,
+    number: number,
+    first: boolean,
+    size?: number,
+    empty?: boolean
+}
+
 export type SaleSum = {
     sellerName: string,
     sum: number
